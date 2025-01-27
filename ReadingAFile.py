711638d67a677f8cba6c39159218941e8f02c57a
Steven Meyer <steven.a.meyer23@outlook.com>
2025-01-27 18:46:59 -0500

    Finished tasks for Reading a File

--- ReadingAFile.py
+++ ReadingAFile.py
@@ -1,3 +1,6 @@
+import os
+print(os.getcwd())
+
 # Computers use file systems to store and retrieve data. Each file is an individual container of related information. If
 # you've ever saved a document, downloaded a song, or even sent an email you've created a file on some computer
 # somewhere.
@@ -11,3 +14,15 @@
 # with open('real_cool_document.txt') as cool_doc:
 #   cool_contents = cool_doc.read()
 # print(cool_contents)
+
+# This opens a file object called cool_doc and creates a new indented block where you can read the contents of the
+# opened file. We then read the contents of the file coo_doc using cool_doc.read() and save the resulting string into
+# the variable cool_contents. Then we print coo_contents, which outputs the statement Wowsers!
+
+# ***INSTRUCTIONS***
+# 1. Use with to open the file welcom.txt. Save the file object as text_file
+with open('welcome.txt') as text_file:
+# 2. Read the contents of text_file and save teh results in text_data
+     text_data = text_file.read()
+# Print out text_data
+print(text_data)
